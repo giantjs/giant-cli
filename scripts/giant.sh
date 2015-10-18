2f0f671d59ffc26cc425910ebd28aca12c9bb509
Dan Stocker <dan.stocker@mangahigh.com>
2015-10-18 14:43:13 +0100

    fixes passing multiple arguments to scripts

--- scripts/giant.sh
+++ scripts/giant.sh
@@ -33,13 +33,13 @@ elif [[ $1 == clear ]]; then
     echo -e "${CYAN}Clearing module packages${NC}"
     $0 batch "rm -f *.tgz"
 elif [[ $1 == build ]]; then
-    if [ $# -lt 2 ]; then
-        echo -e "${CYAN}Building modules${NC}"
-        (cat ./module-sequence.dat | xargs -n 1 giant build)
-    else
-        echo -e "${CYAN}Building module${NC} $2"
+    if [[ $# > 1 && $(cat ./module-sequence.dat | grep -- "$2") ]]; then
+        echo -e "${CYAN}Building module $2${NC} ${@:3}"
         ## passing extra arguments to build script
-        "$DIR/build-distribute.sh" "${@:2}"
+        "$DIR/build-distribute.sh" ${@:2}
+    else
+        echo -e "${CYAN}Building modules${NC} ${@:2}"
+        (cat ./module-sequence.dat | xargs -n 1 -I {} giant build {} ${@:2})
     fi
 elif [[ $1 == run ]]; then
     if [ $# -lt 3 ]; then
